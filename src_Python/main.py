eac4488b1e1f9e648abadb2530b9a12e4921b601
Dennis-van-Gils <vangils.dennis@gmail.com>
2023-07-09 23:45:33 +0200

    Horror: First quiver map results. Faulty.

--- src_Python/main.py
+++ src_Python/main.py
@@ -39,7 +39,10 @@ import_file = "E:/Work/_GitHub_repo/2D-PIV-BOS/test_imgs/PIV_rising_vortex_plume
 # Set the IW sizes for multigrid analysis
 # Subsequent IW sizes should be the exact half of the prev IW size
 # IW_SIZES   = [128 96 64 48 32];
-IW_SIZES = [64, 32]
+# IW_SIZES = [64, 32]
+IW_SIZES = [
+    32,
+]
 IW_OVERLAP = 0.5
 
 # ------------------------------------------------------------------------------
@@ -73,6 +76,10 @@ if __name__ == "__main__":
     nIW_SIZES = len(IW_SIZES)
     IW_grid_As: list[IW_Grid] = []
     IW_grid_Bs: list[IW_Grid] = []
+    VMs_x = []
+    VMs_y = []
+    VMs_dx = []
+    VMs_dy = []
 
     # --------------------------------------------------------------------------
     #   Walk over all interrogation window sizes
@@ -85,18 +92,27 @@ if __name__ == "__main__":
         # Create IW_grid for frame A
         # Create IW_grid for frame B
         IW_grid_A = IW_Grid(img_w, img_h, IW_size, IW_OVERLAP)
-        IW_grid_B = IW_grid_A
+        IW_grid_B = IW_Grid(img_w, img_h, IW_size, IW_OVERLAP)
 
         # Already store IW_grid_A in the multigrid map for the first IW size
         if iIW_size == 1:
             IW_grid_As.append(IW_grid_A)
 
+        # Allocate memory for displacement vector map
+        VM_x = IW_grid_A.x
+        VM_y = IW_grid_A.y
+        VM_dx = np.zeros((IW_grid_A.nIWs_y, IW_grid_A.nIWs_x))
+        VM_dy = np.zeros((IW_grid_A.nIWs_y, IW_grid_A.nIWs_x))
+
         # ----------------------------------------------------------------------
         #   Walk over all IWs
         # ----------------------------------------------------------------------
 
-        for iIW in range(18, IW_grid_A.nIWs):
+        for iIW in range(IW_grid_A.nIWs):
             # print(f"iIW = {iIW}")
+
+            # Turn linear index into matrix indices
+            iIW_y, iIW_x = np.unravel_index(iIW, VM_dx.shape, order="C")
 
             # ------------------------------------------------------------------
             #   Calculate IW of frame B
@@ -117,39 +133,72 @@ if __name__ == "__main__":
                     IW_grid_A.y_1D[iIW],
                 )
 
-                """
+                # Retrieve the pre-shift
+                #
                 # MATLAB equivalent:
-                % Retrieve the pre-shift
-                shift_x = round(VMs{iIW_size - 1}.dx(iIW_parent));    % [px]
-                shift_y = round(VMs{iIW_size - 1}.dy(iIW_parent));    % [px]
-                if isnan(shift_x); shift_x = 0; end
-                if isnan(shift_y); shift_y = 0; end
+                #   shift_x = round(VMs{iIW_size - 1}.dx(iIW_parent));    % [px]
+                #   shift_y = round(VMs{iIW_size - 1}.dy(iIW_parent));    % [px]
+                #   if isnan(shift_x); shift_x = 0; end
+                #   if isnan(shift_y); shift_y = 0; end
 
-                % Calculate new center and range of the shifted IW in frame B
-                IW_grid_B.x(iIW)          = IW_grid_B.x(iIW)          + shift_x;
-                IW_grid_B.y(iIW)          = IW_grid_B.y(iIW)          + shift_y;
-                IW_grid_B.x_range(iIW, :) = IW_grid_B.x_range(iIW, :) + shift_x;
-                IW_grid_B.y_range(iIW, :) = IW_grid_B.y_range(iIW, :) + shift_y;
+                iIW_parent_y, iIW_parent_x = np.unravel_index(
+                    iIW_parent, VMs_dx[-1].shape, order="C"
+                )
+                shift_x = np.round(VMs_dx[-1][iIW_parent_y, iIW_parent_x])
+                shift_y = np.round(VMs_dy[-1][iIW_parent_y, iIW_parent_x])
 
-                % The IW should never be shifted outside of frame B.
-                % When it does, equally resize the IWs of both frames A and B such
-                % that the resized IW of frame B still fits in frame B
-                if IW_grid_B.x_range(iIW, 1) < 1
-                IW_grid_B.x_range(iIW, 1) = 1;
-                IW_grid_A.x_range(iIW, 1) = 1 - shift_x;
-                end
-                if IW_grid_B.y_range(iIW, 1) < 1
-                IW_grid_B.y_range(iIW, 1) = 1;
-                IW_grid_A.y_range(iIW, 1) = 1 - shift_y;
-                end
-                if IW_grid_B.x_range(iIW, 2) > img_w
-                IW_grid_B.x_range(iIW, 2) = img_w;
-                IW_grid_A.x_range(iIW, 2) = img_w - shift_x;
-                end
-                if IW_grid_B.y_range(iIW, 2) > img_h
-                IW_grid_B.y_range(iIW, 2) = img_h;
-                IW_grid_A.y_range(iIW, 2) = img_h - shift_y;
-                """
+                if np.isnan(shift_x):
+                    shift_x = 0
+                if np.isnan(shift_y):
+                    shift_y = 0
+
+                # Calculate new center and range of the shifted IW in frame B
+                #
+                # MATLAB equivalent:
+                #   IW_grid_B.x(iIW)          = IW_grid_B.x(iIW)          + shift_x;
+                #   IW_grid_B.y(iIW)          = IW_grid_B.y(iIW)          + shift_y;
+                #   IW_grid_B.x_range(iIW, :) = IW_grid_B.x_range(iIW, :) + shift_x;
+                #   IW_grid_B.y_range(iIW, :) = IW_grid_B.y_range(iIW, :) + shift_y;
+
+                IW_grid_B.x[iIW_y, iIW_x] = IW_grid_B.x[iIW_y, iIW_x] + shift_x
+                IW_grid_B.y[iIW_y, iIW_x] = IW_grid_B.y[iIW_y, iIW_x] + shift_y
+                IW_grid_B.x_range[iIW, :] = IW_grid_B.x_range[iIW, :] + shift_x
+                IW_grid_B.y_range[iIW, :] = IW_grid_B.y_range[iIW, :] + shift_y
+
+                # The IW should never be shifted outside of frame B.
+                # When it does, equally resize the IWs of both frames A and B
+                # such that the resized IW of frame B still fits in frame B
+                #
+                # MATLAB equivalent:
+                #   if IW_grid_B.x_range(iIW, 1) < 1
+                #     IW_grid_B.x_range(iIW, 1) = 1;
+                #     IW_grid_A.x_range(iIW, 1) = 1 - shift_x;
+                #   end
+                #   if IW_grid_B.y_range(iIW, 1) < 1
+                #     IW_grid_B.y_range(iIW, 1) = 1;
+                #     IW_grid_A.y_range(iIW, 1) = 1 - shift_y;
+                #   end
+                #   if IW_grid_B.x_range(iIW, 2) > img_w
+                #     IW_grid_B.x_range(iIW, 2) = img_w;
+                #     IW_grid_A.x_range(iIW, 2) = img_w - shift_x;
+                #   end
+                #   if IW_grid_B.y_range(iIW, 2) > img_h
+                #     IW_grid_B.y_range(iIW, 2) = img_h;
+                #     IW_grid_A.y_range(iIW, 2) = img_h - shift_y;
+                #   end
+
+                if IW_grid_B.x_range[iIW, 0] < 0:
+                    IW_grid_B.x_range[iIW, 0] = 0
+                    IW_grid_A.x_range[iIW, 0] = -shift_x
+                if IW_grid_B.y_range[iIW, 0] < 0:
+                    IW_grid_B.y_range[iIW, 0] = 0
+                    IW_grid_A.y_range[iIW, 0] = -shift_y
+                if IW_grid_B.x_range[iIW, 1] > img_w - 1:
+                    IW_grid_B.x_range[iIW, 1] = img_w - 1
+                    IW_grid_A.x_range[iIW, 1] = img_w - 1 - shift_x
+                if IW_grid_B.y_range[iIW, 1] > img_h - 1:
+                    IW_grid_B.y_range[iIW, 1] = img_h - 1
+                    IW_grid_A.y_range[iIW, 1] = img_h - 1 - shift_y
 
             # ------------------------------------------------------------------
             #   Retrieve images of IW frame A and IW frame B
@@ -181,8 +230,11 @@ if __name__ == "__main__":
             #      C = C/max(C(:));                % Normalize
             #  end
 
-            C = fftconvolve(img_IW_B, img_IW_A)
-            C = C / np.max(C)
+            if img_IW_A.size == 0:
+                C = np.nan
+            else:
+                C = fftconvolve(img_IW_B, img_IW_A)
+                C = C / np.max(C)
 
             if 0:  # DEBUG flag: Show correlation map
                 if iIW == 0:
@@ -225,24 +277,65 @@ if __name__ == "__main__":
             #     dy = peak_y - floor(size(C, 1)/2 + 1) + shift_y;
             #   end
 
-            iMaxC = np.argmax(C)
-            peak_y, peak_x = np.unravel_index(iMaxC, C.shape, order="C")
-            peak_x = int(peak_x)
-            peak_y = int(peak_y)
+            if np.isnan(C).any():
+                dx = np.nan
+                dy = np.nan
+            else:
+                iMaxC = np.argmax(C)
+                peak_y, peak_x = np.unravel_index(iMaxC, C.shape, order="C")
+                peak_x = int(peak_x)
+                peak_y = int(peak_y)
 
-            # Sub-pixel resolution algorithm, 3-point Gaussian fit
-            peak_x, peak_y = subpx_3pgf_2D(C, peak_x, peak_y)
+                # Sub-pixel resolution algorithm, 3-point Gaussian fit
+                peak_x, peak_y = subpx_3pgf_2D(C, peak_x, peak_y)
 
-            if 0:  # DEBUG flag: Show correlation peak
-                plt.plot(peak_x, peak_y, "xr")
-                plt.plot(peak_x, peak_y, "og")
-                plt.draw()
-                # plt.pause(2)
-                plt.show()
+                if 0:  # DEBUG flag: Show correlation peak
+                    plt.plot(peak_x, peak_y, "xr")
+                    plt.plot(peak_x, peak_y, "og")
+                    plt.draw()
+                    # plt.pause(2)
+                    plt.show()
 
-            # Calculate displacement vector
-            dx = peak_x - C.shape[1] // 2 + 1  # + shift_x
-            dy = peak_y - C.shape[0] // 2 + 1  # + shift_y
+                # Calculate displacement vector
+                dx = peak_x - C.shape[1] // 2 + shift_x
+                dy = peak_y - C.shape[0] // 2 + shift_y
+
+            # Store result in vector map
+            VM_dx[iIW_y, iIW_x] = dx
+            VM_dy[iIW_y, iIW_x] = dy
+
+            # print(f"{dx}, {dy}")
+
+        # -----------------------------------------------------------------------
+        #   Store multigrid maps
+        # ----------------------------------------------------------------------
+
+        IW_grid_As.append(IW_grid_A)
+        IW_grid_Bs.append(IW_grid_B)
+        VMs_x.append(VM_x)
+        VMs_y.append(VM_y)
+        VMs_dx.append(VM_dx)
+        VMs_dy.append(VM_dy)
+
+    # --------------------------------------------------------------------------
+    #   Show original image A with unfiltered vector map on top
+    # --------------------------------------------------------------------------
+    quiverX = 3
+    quiverScale = 0
+
+    if 1:
+        fig = plt.figure()
+        plt.imshow(A, cmap="gray", interpolation="none")
+        plt.quiver(
+            VMs_x[-1],
+            VMs_y[-1],
+            VMs_dx[-1] * quiverX,
+            VMs_dy[-1] * quiverX,
+            color="r",
+            linewidths=2,
+        )
+        # quiverScale, 'r', 'LineWidth', 2)
+        plt.show()
 
     duration = perf_counter() - t_0
     print(f"Finished in {duration:.3f} s")
