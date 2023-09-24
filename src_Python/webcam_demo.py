5e36803bc02e6666e8db2b05c120b329f0c75981
Dennis-van-Gils <vangils.dennis@gmail.com>
2023-09-24 19:44:43 +0200

    Update webcam_demo.py

--- src_Python/webcam_demo.py
+++ src_Python/webcam_demo.py
@@ -4,15 +4,26 @@
 import cv2
 import numpy as np
 
-vid = cv2.VideoCapture(0)
+vid = cv2.VideoCapture(0)  # , cv2.CAP_DSHOW)
+vid.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+vid.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+vid.set(cv2.CAP_PROP_CONTRAST, 31)
+vid.set(cv2.CAP_PROP_SATURATION, 31)
+vid.set(cv2.CAP_PROP_GAIN, 127)
+vid.set(cv2.CAP_PROP_SHARPNESS, 63)
+vid.set(cv2.CAP_PROP_FOCUS, 0)
+vid.set(cv2.CAP_PROP_FPS, 30)
+
+print(vid.get(cv2.CAP_PROP_FPS))
 
 while True:
     # Capture the video frame by frame
     ret, frame = vid.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    np_frame = np.asarray(gray, dtype=np.uint8)
-    np_frame[100:200, 100:200] = 255 - np_frame[100:200, 100:200]
-    cv2.imshow("frame", np_frame)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    frame = np.fliplr(frame)
+    # frame = np.asarray(frame, dtype=np.uint8)
+    # frame[100:200, 100:200] = 255 - frame[100:200, 100:200]
+    cv2.imshow("frame", frame)
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
