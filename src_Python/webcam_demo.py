ad47191f3624cc8f8daa3e8c856a5e7eee765bf3
Dennis van Gils <37263907+Dennis-van-Gils@users.noreply.github.com>
2023-09-25 13:43:09 +0200

    Update webcam_demo.py

--- src_Python/webcam_demo.py
+++ src_Python/webcam_demo.py
@@ -7,9 +7,12 @@ import numpy as np
 resolution = (1280, 720)
 resolution = (1920, 1080)
 
-cap = cv2.VideoCapture(1)  # , cv2.CAP_DSHOW)
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, resolution[0])
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, resolution[1])
+# cap.set(cv2.CAP_PROP_SETTINGS, 1)
+# cap.set(cv2.CAP_PROP_AUTO_WB, 0)
+# cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0)
 # cap.set(cv2.CAP_PROP_CONTRAST, 31)
 # cap.set(cv2.CAP_PROP_SATURATION, 31)
 # cap.set(cv2.CAP_PROP_GAIN, 127)
@@ -20,7 +23,7 @@ cap.set(cv2.CAP_PROP_FRAME_HEIGHT, resolution[1])
 print(f"Target fps: {cap.get(cv2.CAP_PROP_FPS)}")
 
 # Fix window size getting multiplied by the Windows display scaling
-display_scaling = 200  # Set equal to the display scaling used in Windows [%]
+display_scaling = 125  # Set equal to the display scaling used in Windows [%]
 scaled_resolution = [int(x // (display_scaling / 100)) for x in resolution]
 cv2.namedWindow("main", cv2.WINDOW_NORMAL)
 cv2.resizeWindow("main", scaled_resolution[0], scaled_resolution[1])
@@ -28,7 +31,7 @@ cv2.resizeWindow("main", scaled_resolution[0], scaled_resolution[1])
 while True:
     # Capture the video frame by frame
     ret, frame = cap.read()
-    # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     frame = np.fliplr(frame)
     # frame = np.asarray(frame, dtype=np.uint8)
     # frame[100:200, 100:200] = 255 - frame[100:200, 100:200]
