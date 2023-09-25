485e583d45eb60e2c8a8f4f6b239101f7d22067d
Dennis van Gils <37263907+Dennis-van-Gils@users.noreply.github.com>
2023-09-25 18:13:12 +0200

    Webcam demo

--- src_Python/live_capture_webcam.py
+++ src_Python/live_capture_webcam.py
@@ -7,7 +7,7 @@ import numpy as np
 resolution = (1280, 720)
 resolution = (1920, 1080)
 
-cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, resolution[0])
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, resolution[1])
 # cap.set(cv2.CAP_PROP_SETTINGS, 1)
